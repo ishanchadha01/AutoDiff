8ec47ea6405c422b101e6a7939d45f5b630d8cb4
Ishan Chadha <ishanchadha01@gmail.com>
2023-07-30 01:55:58 -0400

    update readme

--- README.md
+++ README.md
@@ -15,14 +15,23 @@ make
 ## Usage
 
 ```C++
-#include "AutoDiff/auto_diff_graph.hpp"
-
 int main() {
-	auto graph = AutoDiffGraph();
-	auto add = Add();
-    // TODO: Actually add parameters here
-	std::cout << "Ran main" << std::endl;
-	return 0;
+    Variable x = Variable(0.9);
+    Variable y = Variable(0.4);
+    Variable z = Variable(1.3);
+    Variable out = z*(x+y);
+    AutoDiffGraph g = AutoDiffGraph(out);
+    g.forward_pass();
+    std::cout << "Values: " << std::endl;
+    for (auto& node_id : g.top_sort) {
+        std::cout << node_id << " " << std::get<double>(g.nodes[node_id]->val) << std::endl;
+    }
+    g.backward_pass();
+    std::cout << "Gradients: " << std::endl;
+    for (auto& node_id : g.top_sort) {
+        std::cout << node_id << " " << std::get<double>(g.nodes[node_id]->gradient) << std::endl;
+    }
+	  return 0;
 }
 ```
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main() {
     Variable x = Variable(0.9);
     Variable y = Variable(0.4);
     Variable z = Variable(1.3);
-    Variable out = (x*y+z)*z + x;
+    Variable out = z*(x+y);
     AutoDiffGraph g = AutoDiffGraph(out);
     g.forward_pass();
     std::cout << "Values: " << std::endl;
@@ -18,23 +18,5 @@ int main() {
     for (auto& node_id : g.top_sort) {
         std::cout << node_id << " " << std::get<double>(g.nodes[node_id]->gradient) << std::endl;
     }
-
-    // Variable v = Variable(0.3);
-    // Variable w = Variable(0.5);
-    // Variable x = Variable(0.7);
-    // Variable y = Variable(1.5);
-    // Variable z = Variable(2.0);
-    // Variable out = (y.power(x+y*z)*w)/v;
-    // AutoDiffGraph g = AutoDiffGraph(out);
-    // g.forward_pass();
-    // std::cout << "Values: " << std::endl;
-    // for (auto& node_id : g.top_sort) {
-    //     std::cout << node_id << " " << std::get<double>(g.nodes[node_id]->val) << std::endl;
-    // }
-    // g.backward_pass();
-    // std::cout << "Gradients: " << std::endl;
-    // for (auto& node_id : g.top_sort) {
-    //     std::cout << node_id << " " << std::get<double>(g.nodes[node_id]->gradient) << std::endl;
-    // }
-	return 0;
+	  return 0;
 }
